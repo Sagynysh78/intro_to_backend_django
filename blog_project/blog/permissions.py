be80f7b1844971fdcd3499a4b88c0a0a1978b36c
Sagynysh78 <asyma0902@gmail.com>
2025-05-08 10:37:28 +0500

    sessia

--- blog_project/blog/permissions.py
+++ blog_project/blog/permissions.py
@@ -1,16 +1,7 @@
 from rest_framework import permissions
 
-
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Разрешение, позволяющее только автору редактировать/удалять объект.
-    Остальные могут только читать.
-    """
-
     def has_object_permission(self, request, view, obj):
-        # Разрешаем GET, HEAD, OPTIONS запросы всем
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Разрешаем изменение/удаление только автору
         return obj.author == request.user
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -5,8 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
